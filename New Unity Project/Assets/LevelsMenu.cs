7bc4bf8ee9f91663cd4029ea8c251dc08a0a3e49
Andolion <danila_andrei97@yahoo.com>
2018-10-02 03:25:11 +0200

    Menus...Menus everywhere
    Added:
    -Main Menu
    -Options Menu
    -Levels Menu
    -5 other levels
    -Door object to pass to the next level

---
+++ New Unity Project/Assets/LevelsMenu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelsMenu : MonoBehaviour {
+
+    public void PlayLevel1()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void PlayLevel2()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void PlayLevel3()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void PlayLevel4()
+    {
+        SceneManager.LoadScene(4);
+    }
+
+    public void PlayLevel5()
+    {
+        SceneManager.LoadScene(5);
+    }
+
+    public void PlayLevel6()
+    {
+        SceneManager.LoadScene(6);
+    }
+}
