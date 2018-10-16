6617b70f926cd8c455e7d6d8daa9d5d75a696287
SaraPolak <sarapolakmail@gmail.com>
2018-10-16 17:01:29 +0200

    start of closet collision script

---
+++ Assets/components/Closet/Scripts/ClosetTrigger.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClosetTrigger : MonoBehaviour
+{
+
+    public GameObject player;
+    public Collider2D closet;
+    public Sprite closetClosed;
+    public Sprite closetOpen;
+    public new SpriteRenderer renderer;
+    public bool collision; 
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D collisionInfo)
+    {
+        if (collisionInfo.gameObject == player)
+        {
+            renderer.sprite = closetOpen;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collisionInfo)
+    {
+        if (collisionInfo.gameObject == player)
+        {
+            renderer.sprite = closetClosed;
+        }
+    }
+}
