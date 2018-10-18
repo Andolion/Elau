33988a4a251fa341ae802bca226af5189ad60f40
Andolion <danila_andrei97@yahoo.com>
2018-10-18 02:04:24 +0200

    Finished the door/clock mechanic
    Save functionality still in progress

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -3,15 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour {
-
-	public void PlayGame()
+public class MainMenu : MonoBehaviour
+{
+    public void ContiueGame()
     {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void PlayGame()
+    { 
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void Back()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
     }
 }
