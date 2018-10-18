47c2047270720290fd2ca443cc3a4647cae8c043
s145469 <luud-@live.nl>
2018-10-18 13:39:13 +0200

    Start work on text to speech

---
+++ Assets/components/Narrator/Scripts/Line.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class Line
+{
+    private AudioSource _audio = new AudioSource();
+    private string _line;
+
+    public Line(string line)
+    {
+        _line = line;
+    }
+
+    public void registerAudio(AudioClip audio)
+    {
+        _audio.clip = audio;
+        this.play();
+    }
+
+    public void play()
+    {
+        _audio.Play();
+    }
+
+    public string getString()
+    {
+        return _line;
+    }
+}
---
+++ Assets/components/Narrator/Scripts/NarratorTrigger.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class NarratorTrigger : MonoBehaviour
+{
+
+    public string line;
+    private Line _line;
+
+    // Use this for initialization
+    void Start()
+    {
+        _line = new Line(line);
+        StartCoroutine(downloadTextToSpeech(_line));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator downloadTextToSpeech(Line line)
+    {
+        using (UnityWebRequest www = UnityWebRequestMultimedia.GetAudioClip(this._getUrl(line.getString()), AudioType.MPEG))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.isNetworkError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                line.registerAudio(DownloadHandlerAudioClip.GetContent(www));
+            }
+        }
+    }
+
+    private string _getUrl(string text)
+    {
+        return "http://translate.google.com/translate_tts?tl=en&q=" + UnityWebRequest.EscapeURL(text) + "&client=tw-ob.mpeg";
+    }
+}
