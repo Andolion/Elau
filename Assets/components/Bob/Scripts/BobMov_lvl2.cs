56cb34d38c00b406704e5f6c17a38b6d838106ef
susanvew@hotmail.com <susanvew@hotmail.com>
2018-10-20 14:23:15 +0200

    Lvl 1 + 2 + 3 (near) done
    Lvl 1 - jumping disabled
    Lvl 2 - jumping enabled, TODO trigger with text to speech + reset of
    location of Bob
    Lvl 3 - chandelier as trigger, door opens upon trigger. Jumping enabled
    by using J key.
    TODO: make door advance to level 4 (for some reason it fails)

---
+++ Assets/components/Bob/Scripts/BobMov_lvl2.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BobMov_lvl2 : MonoBehaviour
+{
+
+    public BobCharacterControl control;
+    float horizontalMove = 0;
+    public float runSpeed = 40;
+    public bool jumpEnabled = true;
+    private bool jump = false;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetKeyDown(KeyCode.J) && jumpEnabled)
+        {
+            jump = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+
+        control.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+
+    }
+}
